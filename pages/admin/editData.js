9fc4075a52d93865fb7ebf2867fdf4f37c79df38
NongKen <kencheew@gmail.com>
2019-10-03 01:40:56 +0700

    add feed

--- pages/admin/editData.js
+++ pages/admin/editData.js
@@ -22,15 +22,67 @@ class Admin extends React.Component {
     super(props)
     this.state = {
       textData: '',
+      feed: '',
       title: '',
       subTitle: '',
-      defaultDay: '1'
+      defaultDay: '1',
+      syncMobile: false
     }
+    rootRef.child('textDb').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ textData: data })
+    })
+    rootRef.child('feed').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ feed: data })
+    })
+    rootRef.child('title').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ title: data })
+    })
+    rootRef.child('subTitle').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ subTitle: data })
+    })
+    rootRef.child('defaultDay').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ defaultDay: data })
+    })
+
+    rootRef.child('feedDelay').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ feedDelay: data })
+    })
+
+    rootRef.child('feedPerPage').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ feedPerPage: data })
+    })
+
+    rootRef.child('feedSize').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ feedSize: data })
+    })
+
+    rootRef.child('feedRowHeight').on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({ feedRowHeight: data })
+    })
   }
 
   onUpdateText() {
-    const textData = this.state.textData
-    rootRef.child('/textDb/').set(textData)
+    if (window.confirm('Are you sure to sync feed tp mobile?')) {
+      const textData = this.state.feed
+      rootRef.child('/textDb/').set(textData)
+    }
+  }
+
+  onUpdateFeed() {
+    const feed = this.state.feed
+    rootRef.child('/feed/').set(feed)
+    if (this.state.syncMobile) {
+      rootRef.child('/textDb/').set(feed)
+    }
   }
 
   onUpdateTitle() {
@@ -48,10 +100,26 @@ class Admin extends React.Component {
     rootRef.child('/defaultDay/').set(defaultDay)
   }
 
-  onUpdateDefaultDay() {
-    const defaultDay = this.state.defaultDay
-    rootRef.child('/defaultDay/').set(defaultDay)
+  onUpdateFeedDelay() {
+    const feedDelay = this.state.feedDelay
+    rootRef.child('/feedDelay/').set(feedDelay)
   }
+
+  onUpdateFeedPerPage() {
+    const feedPerPage = this.state.feedPerPage
+    rootRef.child('/feedPerPage/').set(feedPerPage)
+  }
+
+  onUpdateFeedSize() {
+    const feedSize = this.state.feedSize
+    rootRef.child('/feedSize/').set(feedSize)
+  }
+
+  onUpdateFeedRowHeight() {
+    const feedRowHeight = this.state.feedRowHeight
+    rootRef.child('/feedRowHeight/').set(feedRowHeight)
+  }
+  
 
   render() {
     return (
@@ -68,8 +136,27 @@ class Admin extends React.Component {
         <button onClick={() => this.onUpdateDefaultDay  ()}>Set default day</button>
         <br/>
         <br/>
-        <textarea onChange={(e) => this.setState({ textData: e.target.value})}></textarea>
-        <button onClick={() => this.onUpdateText()}>Update Score</button>
+        <textarea onChange={(e) => this.setState({ feed: e.target.value})} value={this.state.feed}></textarea>
+        <button onClick={() => this.onUpdateFeed()}>Update Feed</button>
+        <br/>
+        <input type="checkbox" onChange={(e) => {this.setState({ syncMobile: e.target.checked })}} checked={this.state.syncMobile}/> check to sync to mobile
+        {/* <button onClick={() => this.onUpdateText()}>Sync to Main Page</button> */}
+        <br/>
+        <br/>
+        <input type="number" style={{ width: '500px'}} onChange={(e) => this.setState({ feedDelay: e.target.value})} value={this.state.feedDelay}></input>
+        <button onClick={() => this.onUpdateFeedDelay  ()}>Set feed delay(ms)</button>
+        <br/>
+        <br/>
+        <input type="number" style={{ width: '500px'}} onChange={(e) => this.setState({ feedPerPage: e.target.value})} value={this.state.feedPerPage}></input>
+        <button onClick={() => this.onUpdateFeedPerPage  ()}>Set feed per page</button>
+        <br/>
+        <br/>
+        <input type="number" style={{ width: '500px'}} onChange={(e) => this.setState({ feedSize: e.target.value})} value={this.state.feedSize}></input>
+        <button onClick={() => this.onUpdateFeedSize  ()}>Set feed size</button>
+        <br/>
+        <br/>
+        <input type="number" style={{ width: '500px'}} onChange={(e) => this.setState({ feedRowHeight: e.target.value})} value={this.state.feedRowHeight}></input>
+        <button onClick={() => this.onUpdateFeedRowHeight  ()}>Set feed row height</button>
       </div>
     )
   }
